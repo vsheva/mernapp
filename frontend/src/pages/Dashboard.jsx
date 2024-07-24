831ed4005c78399a4bde01d8ab6683ab5781c0bf
vsheva <valera.sheva@gmail.com>
2024-07-24 00:08:21 -0400

    displying golas

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import GoalForm from './components/GoalForm';
+import GoalItem from './components/GoalItem';
 import Spinner from './components/Spinner';
 import { getGoals, reset } from '../features/goals/goalSlice';
 
@@ -13,9 +14,10 @@ const Dashboard = () => {
   const { user } = state1;
 
   //const { goals, isLoading, isError, message } = useSelector(state => state.goals);
-  const state2 = useSelector(state => state.goals);
-  console.log('state2', state2);
-  const { isLoading, isError, message } = state2;
+  const { goals, isLoading, isError, message } = useSelector(state => state.goals);
+  console.log('goals', goals);
+  // console.log('state2', state2);
+  // const { goals, isLoading, isError, message } = state2;
 
   useEffect(() => {
     if (isError) {
@@ -44,7 +46,17 @@ const Dashboard = () => {
         <p>Goals Dashboard</p>
       </section>
       <GoalForm />
-      <section className="content"></section>
+      <section className="content">
+        {goals.length > 0 ? (
+          <div className="goals">
+            {goals.map(goal => (
+              <GoalItem key={goal.id} goal={goal} />
+            ))}
+          </div>
+        ) : (
+          <h3>You have not set any goals</h3>
+        )}
+      </section>
     </>
   );
 };
